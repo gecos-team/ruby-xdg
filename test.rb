2eade058f3277633adc4e5265dad61db52f8c71f
Christopher L. Ramsey <christopherlramsey@gmx.us>
2012-09-16 17:36:17 -0400

    Switched to Ruby

---
+++ test.rb
@@ -0,0 +1,18 @@
+import sys, os
+import bang.xdg.applications as a
+import bang.xdg.constants as c
+import bang.xdg.menus as m
+import bang.xdg.icons as i
+import bang.xdg.trash as t
+import PyQt4.QtGui as QtGui
+import PyQt4.QtCore as QtCore
+
+if __name__ == '__main__':
+    
+    # c.test()
+    # print('\n')
+    # i.test()
+    # print('\n')
+    # m.test()
+    # print('\n')
+    # t.test()
